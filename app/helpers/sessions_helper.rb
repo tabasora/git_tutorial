9185a7fb920b4762604d2e1914d63f5659ff7a81
soranetabata <21331085ST@gmail.com>
2019-01-26 10:53:38 +0000

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,7 +8,11 @@ module SessionsHelper
        cookies.permanent.signed[:user_id] = user.id
        cookies.permanent[:remember_token] = user.remember_token
       end
-     
+      
+      def current_user?(user)
+        user == current_user
+      end
+      
       def current_user
        if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -41,6 +45,16 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
      end
+     
+      def redirect_back_or(default)
+      redirect_to(session[:forwarding_url] || default)
+      session.delete(:forwarding_url)
+      end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
      
     
 end
